5021ce3d02c9748bb19efae10cc3bba7ea1c7c1f
Brennan Wallace <brennangoffwallace@gmail.com>
2016-03-26 00:11:52 -0400

    index

--- webserver/server.py
+++ webserver/server.py
@@ -208,7 +208,7 @@ def another():
 def webservice(webserviceurl):
     print "webservice route for: " + webserviceurl
     #webservice info
-    temp_name = g.conn.execute("SELECT name FROM public.webservice ws where ws.url = webserviceurl");
+    temp_name = g.conn.execute("SELECT name FROM public.webservice ws where ws.url = " + webserviceurl);
     #temp_name_2 = temp_name.fetchone()
     #     names.append(result['name'])  # can also be accessed using result[0]
     #     cursor.close()
