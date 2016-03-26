bc074e0939bc181aa5db5f9c7639999d13115479
Brennan Wallace <brennangoffwallace@gmail.com>
2016-03-26 13:47:20 -0400

    webservice

--- webserver/server.py
+++ webserver/server.py
@@ -221,7 +221,7 @@ def webservice(webserviceurl):
       print result['username']
       print result['suctextblob']
       print result['suctime']
-      temp = {username: result['username'], suctextblob: result['suctextblob'], suctime: result['suctime']}
+      temp = {'username': result['username'], 'suctextblob': result['suctextblob'], 'suctime': result['suctime']}
       print temp
       print
       print "bf"
