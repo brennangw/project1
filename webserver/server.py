3df75b1a34aa4c5da149f75fd8258718873a112f
Brennan Wallace <brennangoffwallace@gmail.com>
2016-03-27 01:07:34 -0400

    fixed route

--- webserver/server.py
+++ webserver/server.py
@@ -223,6 +223,7 @@ def webservice(webserviceurl):
 def report(webserviceurl):
     context = dict(url = webserviceurl)
     if request.method == 'POST':
+        print "post"
         print request.form
         return redirect('/')
     return render_template("report.html", **context)
